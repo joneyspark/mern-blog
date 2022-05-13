5e3df675f95198353c1265a8b3daacd12b6351cc
Joney Spark <sparkjoney@gmail.com>
2022-05-14 00:14:50 +0100

    Client React Router / Material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
-
+import "./App.css";
+import Header from "./components/Header";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth";
+import Blogs from "./components/Blogs";
+import UserBlogs from "./components/UserBlogs";
+import BlogDetails from "./components/BlogDetails";
+import AddBlog from "./components/AddBlog";
 function App() {
   return (
-    <div className="App">
-      <h1>App.js</h1>
-    </div>
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blogs/add" element={<AddBlog />} />
+          <Route path="/myblogs" element={<UserBlogs />} />
+          <Route path="/myblogs/:id" element={<BlogDetails />} />
+        </Routes>
+      </main>
+    </React.Fragment>
   );
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import {
+  AppBar,
+  Box,
+  Button,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [value, setValue] = React.useState(0);
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        background:
+          "linear-gradient(90deg, rgba(19,48,79,1) 0%, rgba(0,30,60,1) 48%, rgba(14,72,133,1) 100%)",
+      }}
+    >
+      <Toolbar>
+        <Typography variant="h6" color="inherit">
+          React Blog
+        </Typography>
+        <Box display="flex" marginLeft="auto">
+          <Tabs
+            textColor="inherit"
+            value={value}
+            onChange={(e, val) => {
+              setValue(val);
+            }}
+          >
+            <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
+            <Tab LinkComponent={Link} to="/myblogs" label="Add Blog" />
+          </Tabs>
+        </Box>
+        <Box display="flex" marginLeft="auto">
+          <Button
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="warning"
+          >
+            Login
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="warning"
+          >
+            Signup
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="warning"
+          >
+            Logout
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
